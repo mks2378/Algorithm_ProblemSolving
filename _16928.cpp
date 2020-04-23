4389ab8ef6496de603cf508e9207673c81e22f92
ks_min <mks2378@naver.com>
2020-04-23 22:54:08 +0900

    BOJ_16928(revised)

--- _16928.cpp
+++ _16928.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 #include <queue>
 #include <cstring>
 using namespace std;
@@ -7,12 +6,16 @@ using namespace std;
 #define MAX 101
 
 int N,M;
-int MAP[MAX]; // "snake" or "ladder" 통해 이동하는 곳의 index 저장  
-int dist[MAX]; // 해당 값까지 던진 주사위 최소 횟수  
-int dir[] = {1, 2, 3, 4, 5, 6};
+int Move[MAX];
+int dist[MAX];
+
+int dir[] = {1,2,3,4,5,6};
 
 int bfs(){
-	memset(dist, -1, sizeof(dist));
+	// 최소 이동 횟수가 0이므로 -1로 초기화 해주어
+	// 해당 위치에 방문한 여부 판단  
+	memset(dist,-1,sizeof(dist));
+	
 	queue<int> q;
 	q.push(1);
 	dist[1] = 0;
@@ -21,33 +24,29 @@ int bfs(){
 		int Cur = q.front();
 		q.pop();
 		
+		if(Cur==100)
+			return dist[100];
+		
 		for(int i=0;i<6;i++){
 			int new_Cur = Cur + dir[i];
-			
 			if(new_Cur>100) continue;
-			if(MAP[new_Cur]!=0) new_Cur = MAP[new_Cur];
-			if(dist[new_Cur] == -1){
-				dist[new_Cur] = dist[Cur] + 1;
-				q.push(new_Cur);	
+			if(Move[new_Cur]!=0) new_Cur = Move[new_Cur];			
+			
+			//한 번도 방문하지 않았다면, 최소 이동거리 업데이트  
+			if(dist[new_Cur]==-1){
+				q.push(new_Cur);
+				dist[new_Cur] = dist[Cur]+1;	
 			}
 		}		
-	}
-	
-	return dist[100];	
+	}	
 }
 
 int main(){
 	cin>>N>>M;
-	for(int i=0;i<N;i++){
-		int x,y;
+	for(int i=0;i<N+M;i++){
+		int x, y;
 		cin>>x>>y;
-		MAP[x] = y;
-	}
-	
-	for(int i=0;i<M;i++){
-		int u,v;
-		cin>>u>>v;
-		MAP[u] = v;	
+		Move[x] = y;
 	}
 	
 	cout<<bfs();	
